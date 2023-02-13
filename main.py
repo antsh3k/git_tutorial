4cac2d7610811b0f1a9b5121e427fa4a86f6988a
joshauyeung <110977402+joshauyeung@users.noreply.github.com>
2023-02-13 16:52:17 +0000

    Update main.py
    mario came to say hi

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ print('Hello World!')
 
 print('welcome to the course')
 
+print('hi its me, mario')
+
 # Enter code below:
 ## Feel free to add a line of code below here and create a pull request!
 
