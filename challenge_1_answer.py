79d3c1029225c5e0fda41776f39309c26b0e1e70
Amanda Kiemes <amandakiemes@Amandas-MacBook-Pro.local>
2024-12-16 17:16:52 +0100

    Fixed issue with sorted data

--- challenge_1_answer.py
+++ challenge_1_answer.py
@@ -1,29 +1,23 @@
 import pandas
 
+# Import csv into dataframe, extract dimensions and remove Box Dimensions string from dataframe
 df = pandas.read_csv("list_of_presents.csv")
+df[['Length', 'Width', 'Height']] = df['Box Dimensions'].str.extract('(\d{1,2})x(\d{1,2})x(\d{1,2})').astype(int)
+del df['Box Dimensions']
 
-df[['Length', 'Width', 'Height']] = df['Box Dimensions'].str.extract('(\d{1,2})x(\d{1,2})x(\d{1,2})')
+# Sort dimensions, converting df from a dataframe to a series
+df = df.apply(sorted, axis=1)
 
+# Calculate wrapping paper per box
+wrapping_paper = df.apply(lambda x: (2 * (x[0] * x[1] + x[1] * x[2] + x[2] * x[0])) + (x[0] * x[1]))
 
-df[['Smallest Length', 'Second Smallest Length']] = df.apply(lambda x: pandas.Series(sorted([x['Length'], x['Width'], x['Height']])[:2]), axis=1)
+# Calculate ribbon per box
+ribbon = df.apply(lambda x: (2 * (x[0] + x[1])) + (x[0] * x[1] * x[2]))
 
-df[['Length', 'Width', 'Height', 'Smallest Length', 'Second Smallest Length']] = df[['Length', 'Width', 'Height', 'Smallest Length', 'Second Smallest Length']].astype(int)
+# Sum wrapping paper and ribbon
+wrapping_paper_total = wrapping_paper.sum()
+ribbon_total = ribbon.sum()
 
-df['Surface Area'] = 2 * ((df['Length'] * df['Width']) + (df['Width'] * df['Height']) + (df['Height'] * df['Length']))
-df['Smallest Side'] = df['Smallest Length'] * df['Second Smallest Length']
-df['Wrapping Area'] = df['Surface Area'] + (df['Smallest Length'] * df['Second Smallest Length'])
-
-
-df['Smallest Perimeter'] = 2 * (df['Smallest Length'] + df['Second Smallest Length'])
-df['Volume'] = df['Length'] * df['Width'] * df['Height']
-df['Ribbon Length'] = df['Smallest Perimeter'] + df['Volume']
-
-sums = df.sum(axis=0, numeric_only=True)
-
-wrapping_area_sum = sums['Wrapping Area']
-ribbon_length_sum = sums['Ribbon Length']
-
-
-print(f"The total wrapping paper needed is: {wrapping_area_sum}")
-
-print(f"The total ribbon needed is: {ribbon_length_sum}")
+# Print results
+print(f"The total wrapping paper needed is: {wrapping_paper_total}")
+print(f"The total ribbon needed is: {ribbon_total}")
